8dfd0adb9ad6ac5b6497138c5669bcb65717b951
Aslam Parvej <aslamparvej2@gmail.com>
2022-10-19 00:14:40 +0530

    Design Header

---
+++ header.php
@@ -0,0 +1,44 @@
+<?php
+include "head.php";
+?>
+<link rel="stylesheet" href="css/header.css">
+
+</head>
+
+<body>
+    <header>
+        <a href="./index.php">
+            <p id="logo">Read<span>me</span></p>
+        </a>
+        <nav>
+            <ul>
+                <li><a href="./index.php">Home</a></li>
+                <li><a href="">Category</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="https://aslamparvej.github.io/Portfolio/">About Us</a></li>
+            </ul>
+        </nav>
+        <!-- Hamburger Menu  -->
+        <a href="#side-drawer" class="menu-btn">
+            <span></span>
+            <span></span>
+            <span></span>
+        </a>
+    </header>
+    <aside id="side-drawer">
+        <header>
+        <a href="#" class="menu-btn">
+            <span></span>
+            <span></span>
+            <span></span>
+        </a>
+        </header>
+        <nav id="side-drawer-nav">
+            <ul>
+                <li><a href="./index.php">Home</a></li>
+                <li><a href="">Category</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="https://aslamparvej.github.io/Portfolio/">About Us</a></li>
+            </ul>
+        </nav>
+    </aside>
