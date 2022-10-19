faff335c9e6d9d0ce8577ff89700c83b0bf384f9
Aslam Parvej <aslamparvej2@gmail.com>
2022-10-19 22:13:22 +0530

    Create main section

--- head.php
+++ head.php
@@ -10,4 +10,4 @@
     <link href="https://fonts.googleapis.com/css2?family=Anton&family=Montserrat:wght@400;700&family=Public+Sans:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Readme</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/styles.css" type="text/css" />
--- index.php
+++ index.php
@@ -1,5 +1,68 @@
+<link rel="stylesheet" href="css/main.css">
 <?php
 include "header.php";
-
+?>
+<main>
+    <div class="container">
+        <h2>Web Development Blog</h2>
+        <div class="blog-container">
+            <div class="content-container">
+                <div class="image-container">
+                    <img src="./images/blog-1.jpg" alt="blog image">
+                </div>
+                <div class="text-container">
+                    <h3 class="title">How to Become an App Developer</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, deserunt delectus natus quidem tenetur quibusdam quos veniam, iste alias dolorum necessitatibus quae atque adipisci. Aliquid eius autem consequatur quos tempora .</p>
+                    <div class="btn">
+                        <a href="">Read More.</a>
+                    </div>
+                </div>
+            </div>
+            <div class="content-container">
+                <div class="image-container">
+                    <img src="./images/blog-3.jpg" alt="blog image">
+                </div>
+                <div class="text-container">
+                    <h3 class="title">How to Become an App Developer</h3>
+                    <p>magnam dolor? Aut quae eligendi assumenda voluptates laborum! Unde nihil eius nulla autem vitae nam molestias, accusantium eligendi excepturi nemo harum dignissimos officia voluptatibus tempore tenetur magnam velit sit iusto.</p>
+                    <div class="btn">
+                        <a href="">Read More.</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <h2>App Development Blog</h2>
+        <div class="content-container">
+            <div class="image-container">
+                <img src="./images/blog-2.jpg" alt="blog image">
+            </div>
+            <div class="text-container">
+                <h3 class="title">How to Become an App Developer</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, deserunt delectus natus quidem tenetur quibusdam quos veniam, iste alias dolorum necessitatibus quae atque adipisci.  Aut quae eligendi assumenda voluptates .</p>
+                <div class="btn">
+                    <a href="">Read More.</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <h2>Machine Learning Blog</h2>
+        <div class="content-container">
+            <div class="image-container">
+                <img src="./images/blog-3.jpg" alt="blog image">
+            </div>
+            <div class="text-container">
+                <h3 class="title">How to Become an App Developer</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, deserunt delectus natus quidem tenetur quibusdam quos veniam, iste alias dolorum necessitatibus quae atque.</p>
+                <div class="btn">
+                    <a href="">Read More.</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<?php
 include "footer.php";
 ?>
