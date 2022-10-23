1f007cf9daab0c1ea51ff96f263b08e8befa6158
Aslam Parvej <aslamparvej2@gmail.com>
2022-10-23 21:22:16 +0530

    Created About Us page

---
+++ about.php
@@ -0,0 +1,31 @@
+<?php
+include "head.php";
+?>
+<script src="https://kit.fontawesome.com/8f46f31b06.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="css/about.css">
+<?php
+include "header.php"
+?>
+
+<div class="about-container">
+    <div class="text-container">
+        <h2>Hey, Myself <span class="span-1">Aslam Parvej.</span> <span class="span-2"></span> </h2>
+        <h3>I am Full Stack<span class="span-1"></span><span class="span-2"> Web Developer.</span></h3>
+        <div class="para">
+            <p>I am Mid-level Web developer able to build a Web presence from the ground up - from concept, navigation, layout and programming. Skilled at writing well-designed, testable and efficient code using current best practices in Web development. Fast learner, hard worker and team player who is proficient in an array of scripting languages and multimedia Web tools.</p>
+            <p>I have excellent design & coding skills, as well as an ability to convert requirements into exciting online applications.</p>
+        </div>
+        <div class="social-links">
+            <a href="https://github.com/aslamparvej" target="_blank"><i class="fa-brands fa-square-github fa-2x"></i></a>
+            <a href="https://www.linkedin.com/in/aslam-parvej-2810/" target="_blank"><i class="fa-brands fa-linkedin fa-2x"></i></a>
+            <a href="https://www.instagram.com/aslam._parvej/" target="_blank"><i class="fa-brands fa-instagram fa-2x"></i></i></a>
+        </div>
+    </div>
+    <div class="image-container">
+        <img src="./images/blog-3.jpg" alt="profile-pic">
+    </div>
+</div>
+
+<?php
+include "footer.php"
+?>
