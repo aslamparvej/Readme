6c309a8c9a82061c3b55f8478ad2044023edf951
Aslam Parvej <aslamparvej2@gmail.com>
2022-10-26 12:18:50 +0530

    Creted Blog Post Page

---
+++ blog_post.php
@@ -0,0 +1,30 @@
+ <?php 
+    include "head.php";
+ ?>
+ <link rel="stylesheet" href="./css/blog_post.css">
+ <?php 
+    include "header.php";
+ ?>
+   <main>
+      <div class="blog_post-container">
+         <article>
+            <h1>How to become a web developer</h1>
+            <img src="./images/blog-1.jpg" alt="blog post image">
+            <div class="para-content">
+               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id nisi, quisquam placeat commodi aliquid, corporis quam exercitationem magnam distinctio illo consequuntur ab explicabo. Sequi beatae, placeat obcaecati eligendi corporis ab porro adipisci amet inventore dignissimos, blanditiis sed quisquam dicta voluptatum ex consequuntur, rem sint soluta dolorum quod debitis esse vero?</p>
+               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam ipsa dicta, nesciunt veniam quisquam numquam, esse iure odit a pariatur. Consequatur nesciunt eos blanditiis corporis, dolores nobis assumenda sequi nulla et hic commodi magnam quaerat rerum earum, doloribus tempora quasi praesentium eius debitis aut alias accusamus ipsa, nostrum saepe. Incidunt laborum maiores cum ipsum repellendus sequi iusto doloremque voluptatum eveniet aut? Amet debitis, nesciunt pariatur repellat eligendi veritatis, quis nam ea cum odit numquam magnam labore rerum deserunt autem ducimus error culpa? Deleniti nam, hic ex architecto atque eius vel repellendus similique quo eligendi provident assumenda id explicabo corrupti!</p>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, temporibus iste reprehenderit non alias impedit maxime pariatur qui est quisquam unde autem maiores rem enim, tempore dicta consectetur ipsa! Enim aspernatur, exercitationem facilis quo reiciendis doloribus error accusantium veritatis numquam tenetur, ducimus porro repellat ab est! Doloribus repellendus quaerat dignissimos molestiae magni repudiandae deleniti ratione, illo fuga numquam facere, dolore eaque aspernatur, aut nulla sunt aliquid non laborum quod dolor labore accusantium nemo quo explicabo. Iusto itaque veniam exercitationem alias vitae dignissimos ea illum velit cum nam asperiores, iste quibusdam nesciunt tenetur, sequi suscipit inventore omnis porro quam officiis esse eaque nostrum quidem perspiciatis! Ex fugiat culpa esse quasi sunt aspernatur dignissimos nihil. Sed voluptatibus cumque unde mollitia, laborum quaerat ipsam quas assumenda labore, reiciendis quam dolor iusto rerum iste vero nulla tempore recusandae perspiciatis. Sunt perferendis vitae expedita facilis deleniti sequi maxime excepturi. Consequatur molestias magnam animi ullam ipsam, magni incidunt quae veritatis dolores rerum culpa saepe nostrum, tempora blanditiis dicta reiciendis, explicabo deleniti repellat minus sequi eveniet doloremque. Vel velit labore sit ipsam commodi nemo consectetur impedit aliquam ullam nisi reprehenderit ipsum totam voluptate optio, a soluta mollitia cumque omnis non laborum, animi sapiente officiis explicabo? Nam, rem.</p>
+            </div>
+            <div class="blog-details">
+               <p id="blog-date">Blog Date: 26-10-2022</p>
+               <p id="blog-topic">Topic: Web development</p>
+            </div>
+            <div class="comment-container">
+               <a href="./contact.php"><button>Comment</button></a>
+            </div>
+         </article>
+      </div>
+   </main>
+ <?php 
+    include "footer.php";
+ ?>
