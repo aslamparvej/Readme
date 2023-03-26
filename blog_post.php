26dfa0cf9b71c30ace44a0319e368e17d08b605e
Aslam Parvej <aslamparvej2@gmail.com>
2023-03-26 21:03:58 +0530

    changed fontend

--- blog_post.php
+++ blog_post.php
@@ -1,30 +1,56 @@
- <?php 
-    include "head.php";
- ?>
+ <?php
+
+use function PHPSTORM_META\type;
+
+   include "head.php";
+   include "./contributer/config.php";
+
+   $blogId = (int) $_GET['id'];
+
+   $sql =  "SELECT * FROM blog_post WHERE blog_id = {$blogId}";
+   $result = mysqli_query($conn, $sql);
+
+   if (!$result) {
+      echo "Somethis is wrong!";
+   }
+
+   ?>
  <link rel="stylesheet" href="./css/blog_post.css">
- <?php 
-    include "header.php";
- ?>
-   <main>
-      <div class="blog_post-container">
-         <article>
-            <h1>How to become a web developer</h1>
-            <img src="./images/blog-1.jpg" alt="blog post image">
-            <div class="para-content">
-               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id nisi, quisquam placeat commodi aliquid, corporis quam exercitationem magnam distinctio illo consequuntur ab explicabo. Sequi beatae, placeat obcaecati eligendi corporis ab porro adipisci amet inventore dignissimos, blanditiis sed quisquam dicta voluptatum ex consequuntur, rem sint soluta dolorum quod debitis esse vero?</p>
-               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam ipsa dicta, nesciunt veniam quisquam numquam, esse iure odit a pariatur. Consequatur nesciunt eos blanditiis corporis, dolores nobis assumenda sequi nulla et hic commodi magnam quaerat rerum earum, doloribus tempora quasi praesentium eius debitis aut alias accusamus ipsa, nostrum saepe. Incidunt laborum maiores cum ipsum repellendus sequi iusto doloremque voluptatum eveniet aut? Amet debitis, nesciunt pariatur repellat eligendi veritatis, quis nam ea cum odit numquam magnam labore rerum deserunt autem ducimus error culpa? Deleniti nam, hic ex architecto atque eius vel repellendus similique quo eligendi provident assumenda id explicabo corrupti!</p>
-               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, temporibus iste reprehenderit non alias impedit maxime pariatur qui est quisquam unde autem maiores rem enim, tempore dicta consectetur ipsa! Enim aspernatur, exercitationem facilis quo reiciendis doloribus error accusantium veritatis numquam tenetur, ducimus porro repellat ab est! Doloribus repellendus quaerat dignissimos molestiae magni repudiandae deleniti ratione, illo fuga numquam facere, dolore eaque aspernatur, aut nulla sunt aliquid non laborum quod dolor labore accusantium nemo quo explicabo. Iusto itaque veniam exercitationem alias vitae dignissimos ea illum velit cum nam asperiores, iste quibusdam nesciunt tenetur, sequi suscipit inventore omnis porro quam officiis esse eaque nostrum quidem perspiciatis! Ex fugiat culpa esse quasi sunt aspernatur dignissimos nihil. Sed voluptatibus cumque unde mollitia, laborum quaerat ipsam quas assumenda labore, reiciendis quam dolor iusto rerum iste vero nulla tempore recusandae perspiciatis. Sunt perferendis vitae expedita facilis deleniti sequi maxime excepturi. Consequatur molestias magnam animi ullam ipsam, magni incidunt quae veritatis dolores rerum culpa saepe nostrum, tempora blanditiis dicta reiciendis, explicabo deleniti repellat minus sequi eveniet doloremque. Vel velit labore sit ipsam commodi nemo consectetur impedit aliquam ullam nisi reprehenderit ipsum totam voluptate optio, a soluta mollitia cumque omnis non laborum, animi sapiente officiis explicabo? Nam, rem.</p>
-            </div>
-            <div class="blog-details">
-               <p id="blog-date">Blog Date: 26-10-2022</p>
-               <p id="blog-topic">Topic: Web development</p>
-            </div>
-            <div class="comment-container">
-               <a href="./contact.php"><button>Comment</button></a>
-            </div>
-         </article>
-      </div>
-   </main>
- <?php 
-    include "footer.php";
- ?>
+ <?php
+   include "header.php";
+   ?>
+ <main>
+    <div class="blog_post-container">
+       <article>
+         <?php
+            while ($row = mysqli_fetch_assoc($result)) {
+         ?>
+         <h1><?php echo $row['blog_title'] ?></h1>
+         <img src="./contributer/upload/<?php echo $row['blog_image'];?>" alt="blog post image">
+
+         <div class="para-content">
+            <p><?php echo $row['blog_para1']?></p>
+         </div>
+
+         <div class="blog-details">
+             <p id="blog-date">Blog Date: <?php echo $row['blog_date_and_time'] ?></p>
+             <?php 
+               $category_id = $row['blog_category_id'];
+               $sql1 = "SELECT category_name FROM category WHERE category_id = {$category_id}";
+               $result1 = mysqli_query($conn, $sql1);
+               while($row1 = mysqli_fetch_assoc($result1)){
+             ?>
+             <p id="blog-topic">Topic: <?php echo $row1['category_name']?></p>
+             <?php } ?>
+          </div>
+
+          <?php } ?>
+          <div class="comment-container">
+             <a href="./contact.php"><button>Comment</button></a>
+          </div>
+       </article>
+    </div>
+ </main>
+ <?php
+   include "footer.php";
+   ?>
--- index.php
+++ index.php
@@ -1,38 +1,36 @@
 <link rel="stylesheet" href="css/main.css">
 <?php
 include "header.php";
+include "./contributer/config.php";
+
+$sql =  'SELECT * FROM blog_post';
+$result = mysqli_query($conn, $sql);
+
+if (!$result) {
+    echo "Somethis is wrong!";
+}
 ?>
 <main>
     <div class="container">
-        <h2>Web Development Blog</h2>
-        <div class="blog-container">
-            <div class="content-container">
-                <div class="image-container">
-                    <img src="./images/blog-1.jpg" alt="blog image">
-                </div>
-                <div class="text-container">
-                    <h3 class="title">How to Become an App Developer</h3>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, deserunt delectus natus quidem tenetur quibusdam quos veniam, iste alias dolorum necessitatibus quae atque adipisci. Aliquid eius autem consequatur quos tempora .</p>
-                    <div class="btn">
-                        <a href="./blog_post.php">Read More.</a>
+        <h2>Latest Blog Posts</h2>
+        <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+            <div class="blog-container">
+                <div class="content-container">
+                    <div class="image-container">
+                    <img src="./contributer/upload/<?php echo $row['blog_image'];?>" alt="blog post image">
+                    </div>
+                    <div class="text-container">
+                        <h3 class="title"><?php echo $row['blog_title'] ?></h3>
+                        <p><?php echo substr($row['blog_para1'], 0, 400) ?></p>
+                        <div class="btn">
+                            <a href="./blog_post.php?id=<?php echo $row['blog_id'] ?>">Read More.</a>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="content-container">
-                <div class="image-container">
-                    <img src="./images/blog-3.jpg" alt="blog image">
-                </div>
-                <div class="text-container">
-                    <h3 class="title">How to Become an App Developer</h3>
-                    <p>magnam dolor? Aut quae eligendi assumenda voluptates laborum! Unde nihil eius nulla autem vitae nam molestias, accusantium eligendi excepturi nemo harum dignissimos officia voluptatibus tempore tenetur magnam velit sit iusto.</p>
-                    <div class="btn">
-                        <a href="">Read More.</a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php }  ?>
     </div>
-    <div class="container">
+    <!-- <div class="container">
         <h2>App Development Blog</h2>
         <div class="content-container">
             <div class="image-container">
@@ -40,14 +38,14 @@ include "header.php";
             </div>
             <div class="text-container">
                 <h3 class="title">How to Become an App Developer</h3>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, deserunt delectus natus quidem tenetur quibusdam quos veniam, iste alias dolorum necessitatibus quae atque adipisci.  Aut quae eligendi assumenda voluptates .</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, deserunt delectus natus quidem tenetur quibusdam quos veniam, iste alias dolorum necessitatibus quae atque adipisci. Aut quae eligendi assumenda voluptates .</p>
                 <div class="btn">
                     <a href="">Read More.</a>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="container">
+    </div> -->
+    <!-- <div class="container">
         <h2>Machine Learning Blog</h2>
         <div class="content-container">
             <div class="image-container">
@@ -61,7 +59,7 @@ include "header.php";
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 </main>
 <?php
 include "footer.php";
